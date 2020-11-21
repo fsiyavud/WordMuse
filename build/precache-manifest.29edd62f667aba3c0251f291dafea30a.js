50b60cfeca45805191a2d740278c63c87fed942f
Faisal Siyavudeen <faisal.siyavudeen@gmail.com>
2020-11-22 02:14:31 +0530

    build folder for publishing the page

---
+++ build/precache-manifest.29edd62f667aba3c0251f291dafea30a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba5085ac798787a3e09",
+    "url": "./static/css/main.00cc9d83.chunk.css"
+  },
+  {
+    "revision": "9ba5085ac798787a3e09",
+    "url": "./static/js/main.0651b555.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "df47c003aa785b34ac1e",
+    "url": "./static/js/2.71b73da5.chunk.js"
+  },
+  {
+    "revision": "14fea2ff230360ad0075942e7d83607c",
+    "url": "./index.html"
+  }
+];
